1958029ee3e48677473753e14648b42b60224d65
wallacefw1987 <wallacefw@qq.com>
2017-05-25 22:55:15 +0800

    代码一些bug的update

---
+++ src/main/java/com/fanwei/dao/LearnDao.java
@@ -0,0 +1,18 @@
+package com.fanwei.dao;
+
+
+import com.fanwei.pojo.LearnResource;
+import com.fanwei.tools.Page;
+import java.util.Map;
+
+/**
+ * LearnDao 接口
+ * Created by Administrator on 2017/4/29.
+ */
+public interface LearnDao {
+    Integer add(LearnResource learnResource);
+    Integer update(LearnResource learnResource);
+    Integer deleteByIds(String ids);
+    LearnResource queryLearnResourceById(Integer id);
+    Page queryLearnResourceList(Map<String, Object> params);
+}
---
+++ src/main/java/com/fanwei/pojo/User.java
@@ -0,0 +1,31 @@
+package com.fanwei.pojo;
+
+/**
+ * 登录用户
+ * Created by Administrator on 2017/5/3.
+ */
+public class User {
+    private String name;
+    private String passwrod;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswrod() {
+        return passwrod;
+    }
+
+    public void setPasswrod(String passwrod) {
+        this.passwrod = passwrod;
+    }
+
+    public User(String name, String passwrod) {
+        this.name = name;
+        this.passwrod = passwrod;
+    }
+}
